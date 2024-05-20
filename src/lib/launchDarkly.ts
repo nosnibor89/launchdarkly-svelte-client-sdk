1e2f88d40c25a3dd8c0f39e6caef47d11389b592
Edian Comachio <comachio.edian@gmail.com>
2024-05-20 20:35:49 -0300

    Added JSDocs comments fix typos and improved README

--- src/lib/launchDarkly.ts
+++ src/lib/launchDarkly.ts
@@ -2,7 +2,11 @@ import * as LDClient from 'launchdarkly-js-client-sdk';
 import { writable, derived, type Writable, get, readonly, type Readable } from 'svelte/store';
 import { DEV } from 'esm-env';
 
-//TODO: Add logging strategy
+/**
+ * Logs debug information if in development mode.
+ * @param {() => void} callback - The callback that logs the debug information.
+ * TODO: Add better logging strategy.
+ */
 function debugLog(callback: () => void): void {
 	if (DEV) {
 		callback();
@@ -10,27 +14,48 @@ function debugLog(callback: () => void): void {
 }
 
 type JSONValue = string | number | boolean | { [x: string]: JSONValue } | Array<JSONValue>;
-export type LDClentID = string;
-export type LDContext = LDClient.LDContext;
-export type LDFlagsValue = boolean | string | number | JSONValue;
-type LDFlags = Record<string, LDFlagsValue>;
 
+/** Client ID for LaunchDarkly */
+export type LDClentID = string;
+
+/** Context for LaunchDarkly */
+export type LDContext = LDClient.LDContext;
+
+/** Value of LaunchDarkly flags */
+export type LDFlagsValue = JSONValue;
+
+/** Flags for LaunchDarkly */
+export type LDFlags = Record<string, LDFlagsValue>;
+
+/**
+ * Creates a LaunchDarkly instance.
+ * @returns {Object} The LaunchDarkly instance object.
+ */
 function createLD() {
 	let ldClient: LDClient.LDClient | undefined;
 	const loading = writable(true);
-	const flags = writable<LDFlags>({});
+	const flagsWritable = writable<LDFlags>({});
 
+	/**
+	 * Initializes the LaunchDarkly client.
+	 * @param {LDClentID} clientId - The client ID.
+	 * @param {LDContext} context - The context.
+	 * @returns {Object} An object with the initialization status store.
+	 * @returns {Writable<boolean>} An object with the initialization status store.
+	 */
 	function initialize(clientId: LDClentID, context: LDContext) {
 		debugLog(() => console.log('Initializing LaunchDarkly client'));
+
 		ldClient = LDClient.initialize(clientId, context);
+
 		ldClient.waitUntilReady().then(() => {
 			loading.set(false);
-			flags.set(ldClient!.allFlags());
+			flagsWritable.set(ldClient!.allFlags());
 		});
 
 		ldClient.on('change', (changes) => {
 			debugLog(() => console.log('Flags updated', changes));
-			flags.set(ldClient!.allFlags());
+			flagsWritable.set(ldClient!.allFlags());
 		});
 
 		return {
@@ -38,28 +63,46 @@ function createLD() {
 		};
 	}
 
+	/**
+	 * Identifies the user context.
+	 * @param {LDContext} context - The user context.
+	 * @returns {Promise} A promise that resolves when the user is identified.
+	 */
 	async function identify(context: LDContext) {
-		if (ldClient) {
-			return ldClient.identify(context);
-		}
+		return ldClient?.identify(context);
 	}
+
+	/**
+	 * Watches a flag for changes.
+	 * @param {string} flagKey - The key of the flag to watch.
+	 * @returns {Readable<LDFlagsValue>} A readable store of the flag value.
+	 */
+	const watch = (flagKey: string): Readable<LDFlagsValue> => {
+		return derived<Writable<LDFlags>, LDFlagsValue>(flagsWritable, ($flags) => {
+			debugLog(() => console.log('watching', flagKey, $flags[flagKey], $flags));
+			return $flags[flagKey];
+		});
+	};
+
+	/**
+	 * Checks if a flag is on.
+	 * @param {string} flagKey - The key of the flag to check.
+	 * @returns {boolean} True if the flag is on, false otherwise.
+	 */
+	const isOn = (flagKey: string): boolean => {
+		const currentFlags = get(flagsWritable);
+		return !!currentFlags[flagKey];
+	};
 
 	return {
 		identify,
-		flags: readonly(flags),
+		flags: readonly(flagsWritable),
 		initialize,
-		intializing: readonly(loading),
-		watch: (flagKey: string): Readable<LDFlagsValue> => {
-			return derived<Writable<LDFlags>, LDFlagsValue>(flags, ($flags) => {
-				debugLog(() => console.log('watching', flagKey, $flags[flagKey], $flags));
-				return $flags[flagKey];
-			});
-		},
-		isOn: (flagKey: string): boolean => {
-			const currentFlags = get(flags);
-			return !!currentFlags[flagKey];
-		}
+		initializing: readonly(loading),
+		watch,
+		isOn
 	};
 }
 
+/** The LaunchDarkly instance */
 export const LD = createLD();
